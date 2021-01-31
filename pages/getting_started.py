a030afa3e24251bc01f3a0e99b3b043185da0ef1
Wiratmika <wiratmika@gmail.com>
2021-01-31 11:45:26 +0700

    Clarify some instructions

--- pages/getting_started.py
+++ pages/getting_started.py
@@ -14,10 +14,10 @@ def app():
         """
         ## How to use
         1. Fine-tune "Your capital" input on the sidebar until "Additional capital required" is near your intended value of stock purchases.
-        2. Top-up your brokerage account with at least "Additional capital required" amount and some extra buffer for safety.
+        2. Top-up your brokerage account with at least "Additional capital required" amount.
         3. Press "Purchase according to recommended allocation" and let the magic works.
-        4. Carry on with your life until the next time you want to purchase stocks again; the beauty of index investing is that you don't have to think nor monitor about it, unlike active investing.
+        4. Carry on with your life until the next time you want to purchase stocks again; the beauty of index investing is that you don't have to think about nor monitor it, unlike active investing.
 
-        By default, auto-buy will only purchase stocks whose diff is greater than 0. You may also manually adjust your holdings by sell stocks which price had gone up from your average purchase prices, indicated by negative diff.
+        By default, auto-buy will only purchase stocks that you should buy but not sell stocks that you should sell. You may also manually adjust your holdings by sell stocks which price had gone up from your average purchase prices, indicated by negative differences.
         """
     )
