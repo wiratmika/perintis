eb4f82143ebd249138a99d1b94ceca925a6afc09
Wiratmika <wiratmika@gmail.com>
2021-03-07 14:37:05 +0700

    Update readme

--- pages/getting_started.py
+++ pages/getting_started.py
@@ -5,9 +5,9 @@ from core import get_stockbit_credentials
 
 def app():
     credentials = get_stockbit_credentials()
-    if not credentials[0] or not credentials[1]:
+    if not (credentials[0] and credentials[1]):
         streamlit.subheader(
-            "Warning: Stockbit credentials are not set. Portfolio will not work."
+            "Warning: Stockbit credentials are not set. Automatic portfolio retrieval and purchase will not work."
         )
 
     streamlit.write(
@@ -18,6 +18,6 @@ def app():
         3. Press "Purchase according to recommended allocation" and let the magic works.
         4. Carry on with your life until the next time you want to purchase stocks again; the beauty of index investing is that you don't have to think about nor monitor it, unlike active investing.
 
-        By default, auto-buy will only purchase stocks that you should buy but not sell stocks that you should sell. You may also manually adjust your holdings by sell stocks which price had gone up from your average purchase prices, indicated by negative differences.
+        By default, auto-purchase will only purchase stocks that you should buy but not sell stocks that you should sell. You may also manually adjust your holdings by sell stocks which price had gone up from your average purchase prices, indicated by negative differences.
         """
     )
