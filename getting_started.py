daae3cf815ef6709a248913f4dd67ea716cdd183
Wiratmika <wiratmika@gmail.com>
2022-07-02 09:40:50 +0700

    Add stamp duty reminder

--- getting_started.py
+++ getting_started.py
@@ -14,7 +14,7 @@ def app():
         """
         ## How to use
         1. Fine-tune "Your capital" input on the sidebar until "Additional capital required" is near your intended value of stock purchases.
-        2. Top-up your brokerage account with at least "Additional capital required" amount.
+        2. Top-up your brokerage account with at least "Additional capital required" amount, plus Rp10,000 for stamp duty.
         3. Press "Purchase according to recommended allocation" and let the magic works.
         4. Carry on with your life until the next time you want to purchase stocks again; the beauty of index investing is that you don't have to think about nor monitor it, unlike active investing.
 
